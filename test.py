41facdcc1df721ae38bac0f94a350fdf6d77a24f
Kaushik Vasudevan <KVASU@Kaushiks-MacBook-Pro.local>
2017-12-06 14:50:35 -0800

    [#testbranch] editing test.py program

--- test.py
+++ test.py
@@ -1,4 +1,16 @@
-def testprint():
-	print 'hello world'
+'''
+test.py
+Kaushik 
+12/7/17
+Making changes to test.py file
+'''
+import pandas as pd
 
-testprint()
+class Test():
+    def getData(self, filename):
+        df = pd.read_csv(filename)
+        return df
+
+filename = '~/DataScience/github_projects/Data_Science_Projects/mtg_app_original/MagicDatasets/MagicCards_7_25.csv'
+test = Test()
+test.getData(filename)
